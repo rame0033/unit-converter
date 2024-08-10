7b83435db878cd4df522d7ee996c6683e3c41328
Gian William Ramelb <144856637+rame0033@users.noreply.github.com>
2024-08-10 11:09:21 -0400

    Add conversion function

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const numInput = document.getElementById("num-con")
+const convertBtn = document.getElementById("convert")
+const lengthEl = document.getElementById("con-length")
+const volumeEl = document.getElementById("con-volume")
+const massEl = document.getElementById("con-mass")
+
+// Limit the number of 4 characters in the input field
+numInput.addEventListener("input", function() {
+    if (this.value.length > 5) {
+        this.value = this.value.slice(0, 5);
+    }
+})
+
+// Set conversion values
+const meter2feet = 3.281
+const feet2meter = 0.3048
+const liter2gallon = 0.264
+const gallon2liter = 3.785
+const kg2pound = 2.205
+const pound2kg = 0.4536
+
+
+
+// Conversion functions
+convertBtn.addEventListener("click", function() {
+    
+    try{
+    const baseNum = parseFloat(numInput.value)
+    
+    // Check if the input is a number 
+    if (isNaN(baseNum)) {
+        throw new Error("Please enter a valid number")
+    }
+
+    console.log(`Base number: ${baseNum}`)
+
+    // length conversion (meter to feet and vice versa)
+    lengthEl.textContent = `${baseNum} meters = ${(baseNum * meter2feet).toFixed(2)} feet | ${baseNum} feet = ${(baseNum * feet2meter).toFixed(2)} meters`;
+
+    // volume conversion (liter to gallon and vice versa)
+    volumeEl.textContent = `${baseNum} liters = ${(baseNum * liter2gallon).toFixed(2)} gallons | ${baseNum} gallons = ${(baseNum * gallon2liter).toFixed(2)} liters`;
+
+    // mass conversion (kg to pound and vice versa)
+    massEl.textContent = `${baseNum} kilograms = ${(baseNum * kg2pound).toFixed(2)} pounds | ${baseNum} pounds = ${(baseNum * pound2kg).toFixed(2)} kilograms`;
+    
+    } catch (error) {
+        alert("Please enter a valid number")
+    }
+});
