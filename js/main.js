9655eb38329c32202f9c1f51454dc8cd880d2059
Gian William Ramelb <144856637+rame0033@users.noreply.github.com>
2024-08-10 14:16:03 -0400

    Beautify code format

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const volumeEl = document.getElementById("con-volume")
 const massEl = document.getElementById("con-mass")
 
 // Limit the number of 5 characters in the input field
-numInput.addEventListener("input", function() {
+numInput.addEventListener("input", function () {
     if (this.value.length > 5) {
         this.value = this.value.substring(0, 5);
     }
@@ -20,28 +20,29 @@ const kg2pound = 2.205
 const pound2kg = 0.4536
 
 // Conversion functions
-convertBtn.addEventListener("click", function() {
-    
-    try{
-    // Retrieve the input value
-    const baseNum = parseFloat(numInput.value)
-    
-    // Check if the input is a number 
-    if (isNaN(baseNum)) {
-        throw new Error("Please enter a valid number")
-    }
+convertBtn.addEventListener("click", function () {
 
-    console.log(`Base number: ${baseNum}`)
+    try {
+        // Retrieve the input value
+        const baseNum = parseFloat(numInput.value)
 
-    // length conversion (meter to feet and vice versa)
-    lengthEl.textContent = `${baseNum} meters = ${(baseNum * meter2feet).toFixed(2)} feet | ${baseNum} feet = ${(baseNum * feet2meter).toFixed(2)} meters`;
+        // Check if the input is a number 
+        if (isNaN(baseNum)) {
+            throw new Error("Please enter a valid number")
+        } else {
+            console.log(`Base number: ${baseNum}`)
+        }
 
-    // volume conversion (liter to gallon and vice versa)
-    volumeEl.textContent = `${baseNum} liters = ${(baseNum * liter2gallon).toFixed(2)} gallons | ${baseNum} gallons = ${(baseNum * gallon2liter).toFixed(2)} liters`;
 
-    // mass conversion (kg to pound and vice versa)
-    massEl.textContent = `${baseNum} kilograms = ${(baseNum * kg2pound).toFixed(2)} pounds | ${baseNum} pounds = ${(baseNum * pound2kg).toFixed(2)} kilograms`;
-    
+        // length conversion (meter to feet and vice versa)
+        lengthEl.textContent = `${baseNum} meters = ${(baseNum * meter2feet).toFixed(2)} feet | ${baseNum} feet = ${(baseNum * feet2meter).toFixed(2)} meters`;
+
+        // volume conversion (liter to gallon and vice versa)
+        volumeEl.textContent = `${baseNum} liters = ${(baseNum * liter2gallon).toFixed(2)} gallons | ${baseNum} gallons = ${(baseNum * gallon2liter).toFixed(2)} liters`;
+
+        // mass conversion (kg to pound and vice versa)
+        massEl.textContent = `${baseNum} kilograms = ${(baseNum * kg2pound).toFixed(2)} pounds | ${baseNum} pounds = ${(baseNum * pound2kg).toFixed(2)} kilograms`;
+
     } catch (error) {
         alert("Please enter a valid number")
     }
